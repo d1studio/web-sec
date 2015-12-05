15ee8a25d7e6b8a8379bc1cd88682a4f48075454
程涛 <751753158@qq.com>
2015-12-05 21:31:30 +0800

    添加文章

---
+++ article/php-sec/sqli_blind.php
@@ -0,0 +1,27 @@
+<?php include __DIR__.'/../common/header.html'; ?>
+
+
+
+
+<div class="container">
+    <div class="row">
+        <h1>PHP SQL盲注入<h1>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <p>
+                Sql盲注就是没有很明显的信息反馈的注入方式.
+            </p>
+            <p>
+                
+            </p>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php include __DIR__.'/../common/footer.html'; ?>
---
+++ article/tools/sqlmap.php
@@ -0,0 +1,30 @@
+<?php include __DIR__.'/../common/header.html'; ?>
+
+
+
+
+    <div class="container">
+        <div class="row">
+            <h1>SqlMap 安装<h1>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <p>
+                    <pre>
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+                    </pre>
+                <p>
+
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<?php include __DIR__.'/../common/footer.html'; ?>
