0a873e13c27896f0cf9b698a38d3a1a6ecd334c7
程涛 <751753158@qq.com>
2016-01-06 14:52:23 +0800

    第六期

---
+++ article/kali/nikto.php
@@ -0,0 +1,34 @@
+<?php
+$title = "Kali Linux Nikto";
+include __DIR__.'/../common/header.html';
+?>
+    <div class="container">
+        <div class="row">
+            <h1>Kali Linux Nikto<h1>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>编号</th>
+                        <th>命令</th>
+                        <th>说明</th>
+                    </tr>
+                    </thead>
+                    <tr>
+                        <td class="">...</td>
+                        <td class="">...</td>
+                        <td class="">...</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<?php include __DIR__.'/../common/footer.html'; ?>
